37eeb7226b6ec9e5419f716e650309774031063a
Khahrahk <lolilop.01@mail.ru>
2023-05-09 22:16:14 +0300

    updated todo view page

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -16,29 +16,36 @@
                         @endif
                         <a href="{{url('todos/index')}}" class="btn btn-info">Go back</a>
                         <br>
-                        Title: {{ $todo->title }}
                         <br>
-                        Description: {{ $todo->body }}
+                        <div class="row">
+                            <div class="col-3"><h3>Title: {{ $todo->title }}</h3></div>
+                            <div class="col-8">
+                                <h3 style="display: inline; margin-right: 10px">Tags: </h3>
+                                @foreach($todo->tags as $tag)
+                                    <label class="checkbox-btn">
+                                        <input type="checkbox" disabled>
+                                        <span>{{ $tag->name }}</span>
+                                    </label>
+                                @endforeach
+                            </div>
+                        </div>
                         <br>
-                        Tags:
-                        @foreach($todo->tags as $tag)
-                            {{ $tag->name }}
-                        @endforeach
+                        <h4>Description: {{ $todo->body }}</h4>
                         <br>
-                        Status:
+                        <h4 style="display: inline; margin-right: 10px">Status: </h4>
                         @if($todo->is_active == 1)
                             <a class="btn btn-sm btn-success" href="">completed</a>
                         @else
                             <a class="btn btn-sm btn-danger" href="">not completed</a>
                         @endif
                         <br>
-                        <a href="">Edit</a>
-                        <a href="{{ route('todos.show', $todo->id) }}">View</a>
-                        @if($todo->is_active == 1)
-                            <a href="">Delete</a>
-                        @else
-                            <a href="">Submit</a>
-                        @endif
+                        <br>
+                        <h4>Image</h4>
+                        <a href="{{url($todo->image)}}">
+                            <img src="{{url($todo->image)}}" width="150px" height="150px"
+                                 class="rounded-3"
+                                 style="border: 1px solid rgba(0, 0, 0, 0.3)">
+                        </a>
                     </div>
                 </div>
             </div>
